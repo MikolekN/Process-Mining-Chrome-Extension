d9415334e44c010a9566ef113d0abc46cc6ec5ea
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2023-07-18 21:11:27 +0200

    Reformat & added .gitignore

--- eventlog_visualisation.py
+++ eventlog_visualisation.py
@@ -1,140 +1,137 @@
 import os
 import sqlite3
 import datetime
+import time
 
-import pandas
 import pandas as pd
 import pm4py
-from pm4py.objects.conversion.log import converter as log_converter
-
-ID = 1
-events = []
-cases = []
 
 
-class case:
-    case_id = 0
-    events = []
+class Case:
+    ID = 0
+    cases = []
 
     def __init__(self, case_events=None):
-        global ID, cases
-        self.case_id = ID
-        ID += 1
+        self.case_id = Case.ID
+        Case.ID += 1
         self.events = case_events
-        cases.append(self)
+        Case.cases.append(self)
 
     def __str__(self):
-        string = []
-        for e in self.events:
-            string.append(str(e))
-        return str(self.case_id) + ") " + " -> ".join(string)
+        event_strings = [str(event) for event in self.events]
+        return str(self.case_id) + ") " + " -> ".join(event_strings)
 
 
-class event:
-    event_id = 0
-    timestamp = 0
-    from_visit = 0
-    title = 0
-    url = 0
-    transition = 0
-    duration = 0
-    tip = True
+class Event:
+    events = []
 
-    def __init__(self, id, time, from_visit, title, url, transition, duration):
-        global events
+    def __init__(self, id, t, from_visit, title, url, transition, duration):
         self.event_id = id
-        self.timestamp = time
+        self.timestamp = t
         self.from_visit = from_visit
         self.title = title
         self.url = url
         self.transition = transition
         self.duration = duration
         self.tip = True
-        process_events(from_visit)
-        events.append(self)
+        self.process_events(from_visit)
+        Event.events.append(self)
+
+    @staticmethod
+    def process_events(from_visit):
+        if from_visit != 0:
+            for event in Event.events:
+                if event.event_id == from_visit:
+                    event.tip = False
 
     def __str__(self):
         return str(self.event_id)
 
 
-def process_events(from_visit):
-    if from_visit != 0:
-        for e in events:
-            if e.event_id == from_visit:
-                e.tip = False
-
-
 def get_event(id):
-    for e in events:
-        if e.event_id == id:
-            return e
+    for event in Event.events:
+        if event.event_id == id:
+            return event
     return None
 
 
 def print_events():
-    for e in events:
-        print(e.event_id, e.from_visit, e.tip)
+    for event in Event.events:
+        print(
+            event.event_id,
+            event.from_visit,
+            event.tip
+        )
 
 
 def print_full_events():
-    for e in events:
-        print(e.event_id, e.from_visit, e.tip, e.title, e.url, e.transition, e.duration)
+    for event in Event.events:
+        print(
+            event.event_id,
+            event.from_visit,
+            event.tip,
+            event.title,
+            event.url,
+            event.transition,
+            event.duration
+        )
 
 
 def print_eventlog():
-    print("Case ID".center(10, ' ')
-          + " | " + "Event ID".center(10, ' ')
-          + " | " + "From ID".center(10, ' ')
-          + " | " + "Timestamp".center(20, ' ')
-          + " | " + "Transition".center(12, ' ')
-          + " | " + "Duration".center(10, ' ')
-          + " | " + "Tip".center(10, ' ')
-          + " | " + "Title".center(100, ' ')
-          + " | " + "URL".center(200, ' '))
-    for c in cases:
-        for e in c.events:
-            if e == c.events[0]:
-                print(str(c.case_id).center(10, ' ')
-                      + " | " + str(e.event_id).center(10, ' ')
-                      + " | " + str(e.from_visit).center(10, ' ')
-                      + " | " + str(e.timestamp).center(20, ' ')
-                      + " | " + str(e.transition).center(12, ' ')
-                      + " | " + str(e.duration).center(10, ' ')
-                      + " | " + str(e.tip).center(10, ' ')
-                      + " | " + str(e.title[:100]).ljust(100, ' ')
-                      + " | " + str(e.url).ljust(200, ' '))
+    header = [
+        "Case ID".center(10),
+        "Event ID".center(10),
+        "From ID".center(10),
+        "Timestamp".center(20),
+        "Transition".center(12),
+        "Duration".center(10),
+        "Tip".center(10),
+        "Title".center(100),
+        "URL".center(200)
+    ]
+    print(" | ".join(header))
+
+    for case in Case.cases:
+        for i, event in enumerate(case.events):
+            row = [
+                str(case.case_id).center(10),
+                str(event.event_id).center(10),
+                str(event.from_visit).center(10),
+                str(event.timestamp).center(20),
+                str(event.transition).center(12),
+                str(event.duration).center(10),
+                str(event.tip).center(10),
+                str(event.title[:100]).ljust(100),
+                str(event.url).ljust(200)
+            ]
+            if i == 0:
+                print(" | ".join(row))
             else:
-                print(" ".center(10, ' ')
-                      + " | " + str(e.event_id).center(10, ' ')
-                      + " | " + str(e.from_visit).center(10, ' ')
-                      + " | " + str(e.timestamp).center(20, ' ')
-                      + " | " + str(e.transition).center(12, ' ')
-                      + " | " + str(e.duration).center(10, ' ')
-                      + " | " + str(e.tip).center(10, ' ')
-                      + " | " + str(e.title[:100]).ljust(100, ' ')
-                      + " | " + str(e.url).ljust(200, ' '))
+                row[0] = "".center(10)  # Empty space for Case ID in subsequent rows
+                print(" | ".join(row))
 
 
 def print_cases():
-    for c in cases:
+    for c in Case.cases:
         print(c)
 
 
 def create_cases():
-    for e in events:
+    for e in Event.events:
         c = []
         if e.tip:
             c.append(e)
             current = e
             while current.from_visit != 0:
                 current = get_event(current.from_visit)
+                if current is None:
+                    break
                 c.append(current)
             c.reverse()
-            case(c)
+            Case(c)
 
 
 def load_events():
-    global events
     # path to user's history database (Chrome)
     data_path = os.path.expanduser('~') + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
     files = os.listdir(data_path)
@@ -149,9 +146,9 @@ def load_events():
     cursor.execute(select_statement)
 
     results = cursor.fetchall()  # tuple
-    for event_id, title, url, time, from_visit, transition, duration in results:
-        event(event_id, time, from_visit, title, url, transition, duration)
-    events = events[200:]
+    for event_id, title, url, t, from_visit, transition, duration in results:
+        Event(event_id, t, from_visit, title, url, transition, duration)
+    # events = events[150:]
 
 
 def read_data_to_export():
@@ -164,7 +161,7 @@ def read_data_to_export():
     titles = []
     urls = []
 
-    for c in cases:
+    for c in Case.cases:
         for e in c.events:
             case_ids.append(str(c.case_id))
             event_ids.append(e.event_id)
@@ -194,7 +191,7 @@ def read_data_to_export():
 def export_to_csv():
     df = read_data_to_export()
     df.to_csv("eventlog_CSV.csv")
-    df = pandas.read_csv("eventlog_CSV.csv")
+    df = pd.read_csv("eventlog_CSV.csv")
     print(df.to_string())
 
 
@@ -205,12 +202,26 @@ def export_to_xes():
     print(df.to_string())
 
 
+def in_the_background():
+    raise PermissionError
+    while True:
+        try:
+            load_events()
+        except:
+            time.sleep(5)
+            continue
+        create_cases()
+        print_eventlog()
+        time.sleep(10)
+
+
 load_events()
 create_cases()
 while True:
     print()
     prompt = input("[.] Type:\n[.] <a> to print full events information\n[.] <b> to print events\n[.] <c> to print "
-                   "cases\n[.] <d> to print event log\n[.] <e> to export to csv\n[.] <f> to export to xes\n[>] ")
+                   "cases\n[.] <d> to print event log\n[.] <e> to export to csv\n[.] <f> to export to xes\n[.] <g> to "
+                   "continuously print event log (as if it was done in the background)\n[>] ")
     if prompt == "a":
         print_full_events()
     elif prompt == "b":
@@ -223,5 +234,7 @@ while True:
         export_to_csv()
     elif prompt == "f":
         export_to_xes()
+    elif prompt == "g":
+        in_the_background()
     else:
         break
